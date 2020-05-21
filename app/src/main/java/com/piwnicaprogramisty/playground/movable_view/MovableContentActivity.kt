4d84acea82054fc46196383a60311e82ab23e952
Kamil Jeglikowski <kamil.jeglikowski@acaisoft.com>
2020-05-21 12:31:15 +0200

    PP-2 - Movable view

---
+++ app/src/main/java/com/piwnicaprogramisty/playground/movable_view/MovableContentActivity.kt
@@ -0,0 +1,53 @@
+package com.piwnicaprogramisty.playground.movable_view
+
+import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.piwnicaprogramisty.playground.R
+
+class MovableContentActivity : AppCompatActivity() {
+
+    var dX = 0f
+    var dY = 0f
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_moveable_content);
+
+        val imageView = findViewById<ImageView>(R.id.ivRedAndroid);
+
+        imageView.setOnTouchListener { view, motionEvent ->
+            handleImageTouch(
+                view = view,
+                event = motionEvent
+            )
+        }
+    }
+
+    private fun handleImageTouch(view: View, event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                dX = view.x - event.rawX
+                dY = view.y - event.rawY
+                Log.e("zxc", "x " + dX + " " + view.x + " " + event.rawX)
+                Log.e("zxc", "y " + dY + " " + view.y + " " + event.rawY)
+
+            }
+            MotionEvent.ACTION_MOVE -> {
+                Log.e("zxc", "x"  + (event.rawX + dX))
+                Log.e("zxc", "y " + (event.rawY + dY))
+                view.animate()
+                    .x(event.rawX + dX)
+                    .y(event.rawY + dY)
+                    .setDuration(0)
+                    .start()
+            }
+            else -> return false
+        }
+        return true
+    }
+}
